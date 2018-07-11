41308440063e205babe95fb5090ea6226be09711
Dominik Reisner <reidom1994@gmail.com>
2018-07-11 15:55:03 +0200

    Update LightsweepSprite.h
    jost changes the tabs/space stuff sicne github doesn't format them properly

--- Lightsweep_RelativeSpriteSheetTexCoords/LightsweepSprite.h
+++ Lightsweep_RelativeSpriteSheetTexCoords/LightsweepSprite.h
@@ -7,51 +7,63 @@
 class LightsweepSprite : public FSprite
 {
 public:
-	LightsweepSprite();
+    LightsweepSprite();
 
+
+    //=============================================================================================
+    // Cocos2d-x node creation statics
+public:
+    static LightsweepSprite* CreateWithSpriteFrameName(const std::string& filename, const Vec2& position = Vec2(0, 0));
+private:
+    static LightsweepSprite* CreateWithSpriteFrame(SpriteFrame* spriteFrame);
+
+
+    //=============================================================================================
+    // General functoins
+public:
     void SetInterval(float interval, bool immediate_swipe = true);
     void SetSwipesPerInterval(int8_t swipes);
     void SetTimeScale(float time_scale);
-	void Start(bool immediate_swipe = false);
-	void Stop()
-	{
-		//stop in general  use bool running or something else?
-		//Stop current delegate function
-	}
-
-	static LightsweepSprite* CreateWithSpriteFrameName(const std::string& filename, const Vec2& position = Vec2(0, 0));
+    void Start(bool immediate_swipe = false);
+    void Stop();
 
 private:
-	static LightsweepSprite* CreateWithSpriteFrame(SpriteFrame* spriteFrame);
+    void Swipe();
 
+    
+    //=============================================================================================
+    // Delegate functoins
 private:
-	void Swipe();
+    void OnSwipeFinished();
+    void OnIntervalFinished();
 
-	void OnSwipeFinished();
-	void OnIntervalFinished();
 
+    //=============================================================================================
+    // Derived functions
 protected: 
-		virtual void OnInit() override;
-		virtual void update(float dt) override;
+    virtual void OnInit() override;
+    virtual void update(float dt) override;
 
+
+    //=============================================================================================
+    // Gerneral variables
 private:
-	int32_t gl_lerp_loc_;
+    FTimeDelegate delegate_;
+    FTimeframe timeframe_;
+
+    GLProgramState* glprogramstate_;
+    GLProgram* glp;
+    int32_t gl_lerp_loc_;
+
     float time_scale_;
     float interval_;
     int8_t swipes_per_interval_;
-	int8_t current_interval_swipe_count_;
-
-	bool bImneduateSwipe_ : 1;
-
-	FTimeframe timeframe_;
-	FTimeDelegate delegate_;
-
-	GLProgramState* glprogramstate_;
-	GLProgram* glp;
+    int8_t current_interval_swipe_count_;
+    bool bImmediateSwipe_ : 1;
 
     // COLOR BIT in one color32 calculator
 };
 
-FORCEINLINE void LightsweepSprite::SetInterval(float interval, bool immediate_swipe /* = true */) { interval_ = interval; bImneduateSwipe_ = immediate_swipe; }
+FORCEINLINE void LightsweepSprite::SetInterval(float interval, bool immediate_swipe /* = true */) { interval_ = interval; bImmediateSwipe_ = immediate_swipe; }
 FORCEINLINE void LightsweepSprite::SetSwipesPerInterval(int8_t swipes) { swipes_per_interval_ = swipes; }
 FORCEINLINE void LightsweepSprite::SetTimeScale(float time_scale) { time_scale_ = time_scale; }
